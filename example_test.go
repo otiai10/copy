77876f1ea22d836ec3e833fbb384519e1c741e3b
Hiromu Ochiai <otiai10@gmail.com>
2020-03-28 16:02:16 +0900

    Add an example for `Options.Skip`

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package copy
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func ExampleCopy() {
@@ -15,4 +16,29 @@ func ExampleCopy() {
 	// Output:
 	// Error: <nil>
 	// IsDir: true
+}
+
+func ExampleOptions() {
+
+	err := Copy(
+		"testdata/example",
+		"testdata.copy/example_with_options",
+		Options{
+			Skip: func(src string) bool {
+				return strings.HasSuffix(src, ".git-like")
+			},
+			OnSymlink: func(src string) SymlinkAction {
+				return Skip
+			},
+			AddPermission: 0200,
+		},
+	)
+	fmt.Println("Error:", err)
+	_, err = os.Stat("testdata.copy/example_with_options/.git-like")
+	fmt.Println("Skipped:", os.IsNotExist(err))
+
+	// Output:
+	// Error: <nil>
+	// Skipped: true
+
 }
