87013dc9ab4acc438e89ec21e3a865e345b0b64e
Nicola Murino <nicola.murino@gmail.com>
2020-12-17 13:33:31 +0100

    fix build on FreeBSD

--- stat_times_freebsd.go
+++ stat_times_freebsd.go
@@ -1,6 +1,4 @@
-// +build !windows,!darwin,!freebsd
-
-// TODO: add more runtimes
+// +build freebsd
 
 package copy
 
@@ -14,8 +12,8 @@ func getTimeSpec(info os.FileInfo) timespec {
 	stat := info.Sys().(*syscall.Stat_t)
 	times := timespec{
 		Mtime: info.ModTime(),
-		Atime: time.Unix(stat.Atim.Sec, stat.Atim.Nsec),
-		Ctime: time.Unix(stat.Ctim.Sec, stat.Ctim.Nsec),
+		Atime: time.Unix(stat.Atimespec.Sec, stat.Atimespec.Nsec),
+		Ctime: time.Unix(stat.Ctimespec.Sec, stat.Ctimespec.Nsec),
 	}
 	return times
 }
