95ecaf9a80c16aae38fbd4d3851ec2934ead5c8f
Hiromu OCHIAI <otiai10@gmail.com>
2021-01-23 13:31:08 +0900

    Switching test case following go version

--- all_test.go
+++ all_test.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"runtime"
 	"strings"
 	"testing"
 
@@ -48,31 +46,6 @@ func TestCopy(t *testing.T) {
 	When(t, "specified src is just a file", func(t *testing.T) {
 		err := Copy("test/data/case01/README.md", "test/data.copy/case01/README.md")
 		Expect(t, err).ToBe(nil)
-	})
-
-	When(t, "too long name is given", func(t *testing.T) {
-		dest := "foobar"
-		for i := 0; i < 8; i++ {
-			dest = dest + dest
-		}
-		err := Copy("test/data/case00", filepath.Join("test/data/case00", dest))
-		Expect(t, err).Not().ToBe(nil)
-		Expect(t, err).TypeOf("*fs.PathError")
-	})
-
-	When(t, "try to create not permitted location", func(t *testing.T) {
-		if runtime.GOOS == "windows" || runtime.GOOS == "freebsd" || os.Getenv("TESTCASE") != "" {
-			t.Skipf("FIXME: error IS nil here in Windows and FreeBSD")
-		}
-		err := Copy("test/data/case00", "/case00")
-		Expect(t, err).Not().ToBe(nil)
-		Expect(t, err).TypeOf("*fs.PathError")
-	})
-
-	When(t, "try to create a directory on existing file name", func(t *testing.T) {
-		err := Copy("test/data/case02", "test/data.copy/case00/README.md")
-		Expect(t, err).Not().ToBe(nil)
-		Expect(t, err).TypeOf("*fs.PathError")
 	})
 
 	When(t, "source directory includes symbolic link", func(t *testing.T) {
