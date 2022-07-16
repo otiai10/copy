e008e36785a6915076f869e526c015b25e3700d3
fako1024 <mail+github@f-ko.eu>
2022-07-16 14:49:21 +0200

    Fix compilation on non-64bit architectures and file nomenclature

--- preserve_ltimes.go
+++ preserve_ltimes.go
@@ -1,11 +1,22 @@
-//go:build windows || js
-// +build windows js
+//go:build !windows && !plan9 && !js
+// +build !windows,!plan9,!js
 
 package copy
 
-import "os"
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
 
 func preserveLtimes(srcinfo os.FileInfo, dest string) error {
-	//Unsupported
+	spec := getTimeSpec(srcinfo)
+
+	if err := unix.Lutimes(dest, []unix.Timeval{
+		unix.NsecToTimeval(spec.Atime.UnixNano()),
+		unix.NsecToTimeval(spec.Mtime.UnixNano()),
+	}); err != nil {
+		return err
+	}
 	return nil
 }
--- preserve_ltimes_x.go
+++ preserve_ltimes_x.go
@@ -1,22 +1,11 @@
-//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
-// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
+//go:build windows || js || plan9
+// +build windows js plan9
 
 package copy
 
-import (
-	"os"
-
-	"golang.org/x/sys/unix"
-)
+import "os"
 
 func preserveLtimes(srcinfo os.FileInfo, dest string) error {
-	spec := getTimeSpec(srcinfo)
-
-	if err := unix.Lutimes(dest, []unix.Timeval{
-		{Sec: spec.Atime.Unix(), Usec: spec.Atime.UnixNano() / 1000 % 1000},
-		{Sec: spec.Mtime.Unix(), Usec: spec.Mtime.UnixNano() / 1000 % 1000},
-	}); err != nil {
-		return err
-	}
+	//Unsupported
 	return nil
 }
