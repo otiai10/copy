c8aab6c8c5017d96e3b372f9f69e18d50ec8e65e
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2022-11-29 18:52:54 +0100

    Adapt test case03 to to correctly test relative symlinks

--- test_setup_x.go
+++ test_setup_x.go
@@ -10,7 +10,7 @@ import (
 
 func setup(m *testing.M) {
 	os.MkdirAll("test/data.copy", os.ModePerm)
-	os.Symlink("test/data/case01", "test/data/case03/case01")
+	os.Symlink("../case01", "test/data/case03/case01")
 	os.Chmod("test/data/case07/dir_0555", 0555)
 	os.Chmod("test/data/case07/file_0444", 0444)
 }
